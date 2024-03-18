bd23c865d841a3f0cffaecd60ac06609205c2041
crlotwhite <crlotwhite@gmail.com>
2024-03-19 00:53:02 +0900

    Add Explorer

---
+++ Labeling inspectior/Explorer.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Labeling_inspectior
+{
+    partial class Explorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2 });
+            listView1.Dock = DockStyle.Fill;
+            listView1.Location = new Point(0, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(147, 247);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.MouseDoubleClick += listView1_MouseDoubleClick;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Index";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Label";
+            // 
+            // Explorer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(147, 247);
+            ControlBox = false;
+            Controls.Add(listView1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Explorer";
+            Text = "Explorer";
+            Load += Explorer_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+    }
+}
---
+++ Labeling inspectior/Explorer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labeling_inspectior
+{
+    public partial class Explorer : Form
+    {
+        List<Tuple<int, string[]>> fails;
+        Form2 form2;
+
+        public Explorer(List<Tuple<int, string[]>> fails, Form2 form2)
+        {
+            InitializeComponent();
+            this.fails = fails;
+            this.form2 = form2;
+        }
+
+        private void Explorer_Load(object sender, EventArgs e)
+        {
+            foreach (Tuple<int, string[]> fail in fails)
+            {
+                var li = new ListViewItem(new string[] { fail.Item1.ToString(), fail.Item2[2] });
+                listView1.Items.Add(li);
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var li = listView1.SelectedItems[0];
+            int index = int.Parse(li.SubItems[0].Text);
+            form2.ScrollToIndex(index);
+        }
+    }
+}
